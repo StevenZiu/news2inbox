e838170663890289ceef9e7b46d34c3768d9d89f
stevenzhao <zhwdyx1201@gmail.com>
2020-07-04 13:51:09 +0800

    design db schema

---
+++ data/schema.sql
@@ -0,0 +1,29 @@
+create database if not exists news2inbox;
+
+use News2Inbox;
+
+create table if not exists users (
+  uid int auto_increment,
+  username varchar(50) not null,
+  user_email varchar(100) not null,
+  password varchar(260) not null,
+  primary key(uid),
+  unique(user_email)
+);
+
+create table if not exists subscribe (
+  record_id int auto_increment,
+  countries varchar(10) not null,
+  categories varchar(100) not null,
+  number_of_total_articles int not null,
+  uid int,
+  primary key (record_id),
+  foreign key (uid) references users(uid)
+);
+
+create table if not exists profile (
+  uid int,
+  type enum('paid', 'normal') not null,
+  avatar varchar(300) default null,
+  foreign key (uid) references users(uid)
+)
